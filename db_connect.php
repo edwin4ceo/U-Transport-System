e3d159e2c489effe394f38390c9cdff93e2ca1b1
edwin4ceo <1231203190@student.mmu.edu.my>
2025-11-01 17:46:40 +0800

    a

--- db_connect.php
+++ db_connect.php
@@ -3,8 +3,8 @@
 // Define database connection constants
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', ''); // Your MySQL password, often empty in a local XAMPP/WAMP setup
-define('DB_NAME', 'u_transport'); // The database name you created
+define('DB_PASS', ''); // MySQL password, often empty in a local XAMPP/WAMP setup
+define('DB_NAME', 'u_transport'); // The database name created
 
 // Create a database connection
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
