c65afe27a89931ae59bdc0b87bfb92a81dc89ef4
1231201251 <1231201251@student.mmu.edu.my>
2025-11-26 10:17:05 +0800

    footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+</div> </main>
+
+    <footer>
+        <div class="container">
+            <p>&copy; <?php echo date("Y"); ?> U-Transport System. All Rights Reserved.</p>
+        </div>
+    </footer>
+
+</body>
+</html>
