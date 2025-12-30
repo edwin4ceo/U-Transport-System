1cbd91827907c6ca565c428373b7fee7d1a156f3
ZHEJUN <kelvinng051129@gmail.com>
2025-12-30 17:54:38 +0800

    d_mail_config

--- d_mail_config.php
+++ d_mail_config.php
@@ -6,7 +6,7 @@ return [
     "host"       => "smtp.gmail.com",
     "port"       => 587, // 587 = STARTTLS, 465 = SMTPS
     "username"   => "kelvinng051129@gmail.com",
-    "password"   => "yijt whrb qbzo eodj", // App Password (recommended)
+    "password"   => "yijtwhrbqbzoeodj", // App Password (recommended)
     "from_email" => "kelvinng051129@gmail.com",
     "from_name"  => "U-Transport System",
 ];
