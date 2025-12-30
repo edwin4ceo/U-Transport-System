f391e9a72b34ec70ba57892565a8726f1cd8dd74
ZHEJUN <kelvinng051129@gmail.com>
2025-12-30 17:30:20 +0800

    d_config_mail

---
+++ d_mail_config.php
@@ -0,0 +1,12 @@
+<?php
+// mail_config.php
+// Keep secrets here. Do NOT commit this file to public repositories.
+
+return [
+    "host"       => "smtp.gmail.com",
+    "port"       => 587, // 587 = STARTTLS, 465 = SMTPS
+    "username"   => "kelvinng051129@gmail.com",
+    "password"   => "YOUR_GMAIL_APP_PASSWORD", // App Password (recommended)
+    "from_email" => "kelvinng051129@gmail.com",
+    "from_name"  => "U-Transport System",
+];
