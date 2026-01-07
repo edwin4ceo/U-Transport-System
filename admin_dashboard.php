3cf78b94ea1d66272ef9fad6b33e2cbf802c8260
edwin4ceo <1231203190@student.mmu.edu.my>
2026-01-07 14:25:30 +0800

    update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -123,6 +123,7 @@ $total_bookings = mysqli_fetch_assoc($booking_query)['total'];
 
         <div class="stats-grid">
             
+        <a href="view_drivers.php" style="text-decoration:none; color:inherit;">
             <div class="stat-card">
                 <div class="stat-info">
                     <h3><?php echo $total_drivers; ?></h3>
@@ -130,6 +131,7 @@ $total_bookings = mysqli_fetch_assoc($booking_query)['total'];
                 </div>
                 <div class="stat-icon bg-blue"><i class="fa-solid fa-car"></i></div>
             </div>
+        </a>
 
             <a href="verify_drivers.php" style="text-decoration:none; color:inherit;">
                 <div class="stat-card">
@@ -141,6 +143,7 @@ $total_bookings = mysqli_fetch_assoc($booking_query)['total'];
                 </div>
             </a>
 
+            <a href="view_passengers.php" style="text-decoration:none; color:inherit;">
             <div class="stat-card">
                 <div class="stat-info">
                     <h3><?php echo $total_passengers; ?></h3>
@@ -148,7 +151,9 @@ $total_bookings = mysqli_fetch_assoc($booking_query)['total'];
                 </div>
                 <div class="stat-icon bg-purple"><i class="fa-solid fa-users"></i></div>
             </div>
+            </a>
 
+            <a href="view_bookings.php" style="text-decoration:none; color:inherit;">
             <div class="stat-card">
                 <div class="stat-info">
                     <h3><?php echo $total_bookings; ?></h3>
@@ -156,6 +161,7 @@ $total_bookings = mysqli_fetch_assoc($booking_query)['total'];
                 </div>
                 <div class="stat-icon bg-green"><i class="fa-solid fa-calendar-check"></i></div>
             </div>
+            </a>
 
         </div>
 
